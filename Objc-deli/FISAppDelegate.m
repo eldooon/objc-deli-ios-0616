45041212a5e056d941973e6c4a5ad1f9a27c3505
eldooon <eldonchan91@gmail.com>
2016-05-12 15:02:35 -0400

    Done.

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -10,10 +10,41 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *)stringWithDeliLine:(NSArray *)deliLine{
+    
+    if ([deliLine count] == 0){ //if deli is empty
+        NSString *empty = @"The line is currently empty."; //output empty string
+        
+        return empty; //return string
+    }
+    
+    else {
+        
+        NSMutableString *notEmpty = [[NSMutableString alloc]initWithString: @"The line is:"]; //create empty mutable string
+        
+        for (NSUInteger i = 0; i < [deliLine count]; i++){ //create FOR loop to output mutiple name and number
+            
+            NSInteger number = i + 1; //indicate index starts from 1
+            NSString *name = deliLine[i]; //declare name string as each deliLine index
+            [notEmpty appendFormat: @"\n%li. %@", number, name]; // append notempty with number and name
+        }
+        
+        return notEmpty; //return new appended notEmpty string
+    }
+    
+}
+-()addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine{
+    
+    [deliLine addObject:name]; //add name object to deliLine
+    
+    return deliLine;
+}
+-(NSString *)serveNextCustomerInDeliLine:(NSMutableArray *)deliLine{
+    
+    NSString *firstName = deliLine[0]; //declare first name in deliLine as string firstName
+    [deliLine removeObjectAtIndex:0]; //remove first index from deliLine
+    
+    return firstName; //return the string containing first name
+}
 
 @end
